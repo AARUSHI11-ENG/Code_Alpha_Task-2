eacbebd9e6d86c4cb879164cd7c6e3b854484cc5
AARUSHI JHAWAR <aarushijhawar75@gmail.com>
2026-06-11 11:23:23 +0530

    Add search functionality to news API endpoint

--- api/news.js
+++ api/news.js
@@ -3,13 +3,28 @@ export default async function handler(req, res) {
   const category =
   req.query.category || "general";
 
+  const search =
+  req.query.search;
+
   const API_KEY =
   "10fe329dbe4866c984a08d0f4b5b4b5f";
 
+  let url;
+
+  if(search){
+
+    url =
+    `https://gnews.io/api/v4/search?q=${search}&lang=en&country=in&max=20&apikey=${API_KEY}`;
+
+  }else{
+
+    url =
+    `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&max=20&apikey=${API_KEY}`;
+
+  }
+
   const response =
-  await fetch(
-  `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&max=20&apikey=${API_KEY}`
-  );
+  await fetch(url);
 
   const data =
   await response.json();
